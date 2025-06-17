26b666d705c70f9311906bd36ab4739056ad9b40
Antonio Meza <52209606+novamar-programmer2@users.noreply.github.com>
2025-06-17 10:42:16 -0600

    credo screens buttons y cards con name para rutas

---
+++ lib/presentation/screens/cards/card_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardScreen extends StatelessWidget {
+  static const String name = 'cards_screen';
+
+  const CardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("cards Screen")),
+      body: const Placeholder(),
+    );
+  }
+}
