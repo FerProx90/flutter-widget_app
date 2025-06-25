f7cb8342f131e95f109ed4146704a82c686c7c8d
Antonio Meza <52209606+novamar-programmer2@users.noreply.github.com>
2025-06-25 10:28:45 -0600

    creado de drawer con menu

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
+import 'package:widget_app/presentation/widgets/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -12,6 +13,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Home Screen")),
       body: const _HomeView(),
+      drawer: const SideMenu()
     );
   }
 }
---
+++ lib/presentation/widgets/side_menu.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:widget_app/config/menu/menu_items.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  int navDrawerIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.viewPaddingOf(context).top > 35;
+
+    return NavigationDrawer(
+      selectedIndex: navDrawerIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          navDrawerIndex = value;
+        });
+      },
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 28, 16, 10),
+          child: const Text('Main'),
+        ),
+
+        ...appMenuItems.sublist(0, 3).map((menuItem) {
+          return NavigationDrawerDestination(
+            icon: Icon(menuItem.icon),
+            label: Text(menuItem.title),
+          );
+        }),
+
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
+        ),
+
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Text('More options'),
+        ),
+  
+        ...appMenuItems.sublist(4).map((menuItem) {
+          return NavigationDrawerDestination(
+            icon: Icon(menuItem.icon),
+            label: Text(menuItem.title),
+          );
+        }),
+      ],
+    );
+  }
+}
